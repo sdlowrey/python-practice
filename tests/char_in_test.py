ce7eccdf1377ffc0e343f45e174f7871cc42c640
Scott Lowrey <sdlowrey@gmail.com>
2018-05-14 13:29:04 -0500

    char_in one more test

--- tests/char_in_test.py
+++ tests/char_in_test.py
@@ -1,15 +1,24 @@
+from datetime import datetime
 from unittest import TestCase, main
 
 from char_in.main import target_year
 
 
 class TestUserInput(TestCase):
-    def test_century_year(self):
-        current_year = 2000
+    def test_base_year(self):
+        base_year = 2000
         current_age = 10
         target_age = 50
         wanted = 2040
-        got = target_year(current_age, target_age, base_year=current_year)
+        got = target_year(current_age, target_age, base_year=base_year)
+        self.assertEqual(wanted, got)
+
+    def test_this_year(self):
+        current_year = datetime.now().year
+        current_age = 30
+        target_age = 50
+        wanted = current_year + 20
+        got = target_year(current_age, target_age)
         self.assertEqual(wanted, got)
 
 
