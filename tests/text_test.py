2c38057bba81d9dbf33fe451772c5e6e3c45277f
Scott Lowrey <sdlowrey@gmail.com>
2018-05-16 06:38:15 -0500

    rename test data dir, add word test

--- tests/text_test.py
+++ tests/text_test.py
@@ -6,8 +6,15 @@ from text.words import build_index
 
 class WordTest(TestCase):
     def test_small(self):
-        path = os.path.join(os.getcwd(), 'tests/files/small.txt')
+        path = os.path.join(os.getcwd(), 'tests/data/small.txt')
         with open(path) as fp:
             index = build_index(fp)
         self.assertIn('thing', index.keys())
         self.assertEqual(len(index['thing']), 2)
+
+    def test_medium(self):
+        path = os.path.join(os.getcwd(), 'tests/data/medium.txt')
+        with open(path) as fp:
+            index = build_index(fp)
+        self.assertIn('ipsum', index.keys())
+        self.assertEqual(len(index['ipsum']), 6)
