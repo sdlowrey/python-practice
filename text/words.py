fcf58330bd708a53c79fdffc1c1249e97ac070ba
Scott Lowrey <sdlowrey@gmail.com>
2018-05-16 06:54:50 -0500

    use defaultdict for word index

--- text/words.py
+++ text/words.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import re
 from typing import Dict, Iterable, List, Tuple
 
@@ -7,11 +8,11 @@ WORD_RE = re.compile(r'\w+')
 def build_index(text: Iterable) -> Dict[str, List[Tuple[int, int]]]:
     """Create an index of word counts from the input file.
     """
-    index = {}
+    index = defaultdict(list)
     for line_no, line in enumerate(text, 1):
         for match in WORD_RE.finditer(line):
             word = match.group()
             column_no = match.start() + 1
             location = (line_no, column_no)
-            index.setdefault(word, []).append(location)
+            index[word].append(location)
     return index
