0ded6b9ddf1d4fc2ae8e1a3502c6901dea102019
Scott Lowrey <scott.lowrey@fidelissecurity.com>
2021-10-15 14:18:55 -0500

    exclude non-alpha characters

--- text/letters.py
+++ text/letters.py
@@ -5,6 +5,7 @@ Do not consider upper/lower case or non-letters.
 """
 from collections import defaultdict
 from typing import Dict
+import re
 import sys
 
 
@@ -13,12 +14,16 @@ def count_characters(in_str: str) -> Dict:
     """
     counts = defaultdict(int)
     for c in in_str:
+        if not c.isalpha():
+            continue
         counts[c.lower()] += 1
     return counts
 
 
 if __name__ == '__main__':
-    arg1 = sys.argv[1]
-    chars = count_characters(arg1)
-    for symbol, count in chars.items():
+    # (unsafely) assume the first arg is a string
+    in_text = sys.argv[1]
+    print(f'input: {in_text}')
+    counts = count_characters(in_text)
+    for symbol, count in counts.items():
         print(f'{symbol}: {count}')
